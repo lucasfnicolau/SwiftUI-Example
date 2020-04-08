ce453e3b68d513b8cface3092e1507abb1215021
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-04-08 16:33:08 -0300

    Initial Commit

---
+++ Singleton-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Singleton-SwiftUI
+//
+//  Created by Lucas Fernandez Nicolau on 08/04/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
